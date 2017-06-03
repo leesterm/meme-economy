2f3648fd5d4efd3cbbbc662ac0006bed41b007ed
leesterm <leesterm@gmail.com>
2017-06-03 14:04:29 -0400

    create meme controller

---
+++ app/controllers/meme_controller.rb
@@ -0,0 +1,7 @@
+class MemeController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'meme/index'
+
+  get 'meme/show'
+
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
