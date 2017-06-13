c66846d436251913e42e349bb5a03777906db6c5
leesterm <leesterm@gmail.com>
2017-06-12 20:46:17 -0400

    add missing key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.secret_key = '7abc94474eb7ef01d09447177d5db92366b3553be5d7d6e1ad151aec870d4ee5b2a8b6b85825a08d9d76e9eef5f41374628189e2feffd46f0e781403383971c1'
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
